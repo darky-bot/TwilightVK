e694024f915ca48c90236ac5b15a88f5a10a86b3
darkywings <theskanimbrony@gmail.com>
2025-11-24 09:02:43 +0300

    Examples update

--- examples/simplebot.py
+++ examples/simplebot.py
@@ -1,10 +1,14 @@
+import dotenv, os
+
 import twilight_vk
 from twilight_vk.framework.rules import TextRule
 
+dotenv.load_dotenv()
+
 bot = twilight_vk.TwilightVK(
-    BOT_NAME="SimpleBot",
-    ACCESS_TOKEN="123",
-    GROUP_ID=123
+    BOT_NAME=os.getenv("BOT_NAME"),
+    ACCESS_TOKEN=os.getenv("BOT_ACCESS_TOKEN"),
+    GROUP_ID=os.getenv("BOT_GROUP_ID")
 )
 
 @bot.on_event.message_new(TextRule(value=["привет", "hello"], ignore_case=True))
