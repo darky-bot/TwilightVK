7120daa9f959d16101aba94f8c6ba7b91bc36907
darkywings <theskanimbrony@gmail.com>
2026-01-17 18:36:28 +0300

    More event handlers was added

--- twilight_vk/utils/config.py
+++ twilight_vk/utils/config.py
@@ -7,7 +7,7 @@ from ..logger.formatters import (
 class CONFIG:
 
     class FRAMEWORK:
-        version = "0.1.0-beta2"
+        version = "0.1.0-beta3"
         developer = "darky_wings"
     
     class VK_API:
