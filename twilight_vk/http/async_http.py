ad211ed271906810f6814806178a463079882756
darkywings <theskanimbrony@gmail.com>
2026-06-18 07:18:53 +0300

    get method supports data field

--- twilight_vk/http/async_http.py
+++ twilight_vk/http/async_http.py
@@ -33,17 +33,21 @@ class Http:
         return await response.json()
 
     async def get(self,
-                  url:str,
-                  params:dict|None=None,
-                  headers:dict|None=None,
-                  raw:bool=True) -> ClientResponse | dict:
+                  url: str,
+                  data: dict = {},
+                  params: dict | None = None,
+                  headers: dict | None = None,
+                  raw: bool = True) -> ClientResponse | dict:
         '''
         HTTP GET method
 
         :param url: The url to get the response from
         :type url: str
 
-        :param params: Dictionary containing the optional data to be sent in the GET request
+        :param data: Dictionary containing the data to be sent in the request body
+        :type data: dict
+
+        :param params: Dictionary containing the optional data to be sent with the request
         :type params: dict | None
 
         :param headers: Optional dictionary of HTTP headers to include in the request
@@ -56,6 +60,7 @@ class Http:
         try:
             response = await self.session.get(url=url,
                                             params=params,
+                                            json=data,
                                             headers=headers)
         except asyncio.TimeoutError:
             self._retry_attempt += 1
@@ -63,7 +68,7 @@ class Http:
 
             if self._retry_attempt <= self.retries:
                 logger.info(f"Retrying (Attempt ({self._retry_attempt}/{self.retries}))...")
-                return await self.get(url = url, params = params, headers = headers, raw = raw)
+                return await self.get(url = url, data = data, params = params, headers = headers, raw = raw)
             
             logger.error(f"No more retry attempts, raising exception...")
             raise asyncio.TimeoutError
@@ -71,19 +76,22 @@ class Http:
         return await self._is_raw(response, raw=raw)
     
     async def post(self,
-                   url:str,
-                   data:dict,
-                   params:dict={},
-                   headers:dict=None,
-                   raw:bool=True) -> ClientResponse | dict:
+                   url: str,
+                   data: dict = {},
+                   params: dict = {},
+                   headers: dict = None,
+                   raw: bool = True) -> ClientResponse | dict:
         '''
         HTTP POST method
 
         :param url: The URL to send the POST request to
         :type url: str
 
-        :param data: Dictionary containing the data to be sent in the POST request body
+        :param data: Dictionary containing the data to be sent in the request body
         :type data: dict
+
+        :param params: Dictionary containing the optional data to be sent with the request
+        :type params: dict | None
 
         :param headers: Optional dictionary of HTTP headers to include in the request
         :type headers: dict, optional
@@ -103,7 +111,7 @@ class Http:
 
             if self._retry_attempt <= self.retries:
                 logger.info(f"Retrying (Attempt ({self._retry_attempt}/{self.retries}))...")
-                return await self.get(url = url, params = params, headers = headers, raw = raw)
+                return await self.post(url = url, data = data, params = params, headers = headers, raw = raw)
             
             logger.error(f"No more retry attempts, raising exception...")
             raise asyncio.TimeoutError
